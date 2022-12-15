bee714e1f650d32349287326a524b4e24dedfb13
ScobarDen <gdv-ps@yandex.ru>
2022-12-15 19:40:33 +0300

    добавил картинки

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,18 @@
+import pizzaLogoSvg from '../assets/img/pizza-logo.svg';
+
 function Header() {
   return (
     <div className="header">
       <div className="container">
         <div className="header__logo">
-          <img width="38" src="./img/pizza-logo.svg" alt="Pizza logo" />
+          <img width="38" src={pizzaLogoSvg} alt="Pizza logo" />
           <div>
             <h1>React Pizza</h1>
             <p>самая вкусная пицца во вселенной</p>
           </div>
         </div>
         <div className="header__cart">
-          <a href="/cart.html" className="button button--cart">
+          <a href="img/cart.html" className="button button--cart">
             <span>520 ₽</span>
             <div className="button__delimiter"></div>
             <svg
