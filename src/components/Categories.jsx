fd24ae21feab934ce39632fac4204c6710c969c7
ScobarDen <gdv-ps@mail.ru>
2022-12-16 10:44:57 +0300

    рендер списка

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,24 @@
+import { useState } from 'react';
+
 function Categories() {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+
+  const onClickCategory = (i) => {
+    setActiveIndex(i);
+  };
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((category, index) => (
+          <li
+            key={index}
+            className={activeIndex === index ? 'active' : ''}
+            onClick={() => onClickCategory(index)}>
+            {category}
+          </li>
+        ))}
       </ul>
     </div>
   );
