4b88f8c9090f4bccfee6332df4a1f6ed8713591d
ScobarDen <gdv-ps@mail.ru>
2022-12-20 10:58:08 +0300

    добавил главную страницу и начал делать роутинг

--- src/App.js
+++ src/App.js
@@ -1,39 +1,14 @@
 import './scss/app.scss';
 import Header from './components/Header';
-import Categories from './components/Categories';
-import Sort from './components/Sort';
-import PizzaBlock from './components/PizzaBlock';
-import { useEffect, useState } from 'react';
-import Skeleton from './components/PizzaBlock/Skeleton';
+import Home from './pages/Home';
 
 function App() {
-  const [items, setItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  useEffect(() => {
-    setIsLoading(true);
-    fetch('https://63a096f1e3113e5a5c41fd8d.mockapi.io/items')
-      .then((res) => res.json())
-      .then((data) => {
-        setItems(data);
-        setIsLoading(false);
-      });
-  }, []);
-
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {isLoading
-              ? [...new Array(9)].map((_, i) => <Skeleton key={i} />)
-              : items.map((pizza) => <PizzaBlock key={pizza.id} {...pizza} />)}
-          </div>
+          <Home />
         </div>
       </div>
     </div>
