7e9e83b7e760a7c02a7e042f118073f8f6c2dab3
DenScobar <gdv201099@gmail.com>
2022-12-29 10:57:02 +0300

    full pizza taksiak

--- src/pages/FullPizza/index.jsx
+++ src/pages/FullPizza/index.jsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import styles from './FullPizza.module.scss';
@@ -6,7 +6,8 @@ import styles from './FullPizza.module.scss';
 function FullPizza() {
   const { id } = useParams();
   const [pizza, setPizza] = useState({});
-  const { title, price, imageUrl, sizes, types, rating } = pizza;
+  const typeNames = ['тонкое', 'традиционное'];
+
   useEffect(() => {
     async function fetchPizza() {
       try {
@@ -15,23 +16,31 @@ function FullPizza() {
         );
         setPizza(data[0]);
       } catch (e) {
-        console.error(e.message);
+        // console.log(e.message);
       }
     }
-    fetchPizza();
-  }, []);
 
-  if (!pizza) return 'Loading...';
+    fetchPizza();
+  }, [id]);
+
+  if (!pizza.sizes) return 'Loading...';
 
   return (
-    <div>
-      <div><img src={imageUrl} alt="pi"/></div>
-      <div>
-        <h1>{title}</h1>
-        <p>Цена: {price}</p>
-        <p>Размеры: {sizes}</p>
-        <p>Типы: {types}</p>
-        <p>Рейтинг: {rating}</p>
+    <div className={styles.root}>
+      <img src={pizza.imageUrl} alt="pi" />
+      <div className={styles.right}>
+        <div>
+          <h1>{pizza.title}</h1>
+          <h3>Размеры: {pizza.sizes.map(size => <span key={size}>{size} см </span>)}</h3>
+          <h3>Типы: {pizza.types.map(type => <span key={type}>{typeNames[type]} тесто </span>)}</h3>
+          <h2>Рейтинг: {pizza.rating}</h2>
+        </div>
+        <div className={styles.bottom}>
+          <div className={styles.price}>от {pizza.price} ₽</div>
+          <Link to="/" className="button button--black">
+            <span>Вернуться назад</span>
+          </Link>
+        </div>
       </div>
     </div>
   );
